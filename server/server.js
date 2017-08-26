23c7409ea34b95a83a5997ea90b7b50f02fd4342
Root@Git <fml.wow.paul@gmail.com>
2017-08-26 15:45:06 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+var restify = require('restify');
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+    next();
+}
+
+var server = restify.createServer();
+
+server.get('/something/:name', function(request, response, next) {
+    console.log("request.params", request.url); //
+
+});
+
+
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
